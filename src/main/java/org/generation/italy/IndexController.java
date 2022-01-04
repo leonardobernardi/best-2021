48c67bf70e54ad1d0fb43e81934b012fefe08c30
leonardobernardi <lbernardi4298@gmail.com>
2022-01-04 16:18:15 +0100

    commit

--- src/main/java/org/generation/italy/IndexController.java
+++ src/main/java/org/generation/italy/IndexController.java
@@ -28,21 +28,42 @@ public class IndexController {
 		Map<String, String> dune = new HashMap<>();
 		Map<String, String> theLastDuel = new HashMap<>();
 		Map<String, String> theGreenKnight = new HashMap<>();
-		dune.put("Dune", "https://mr.comingsoon.it/imgdb/locandine/235x336/55957.jpg");
-		theLastDuel.put("The Last Duel", "https://pad.mymovies.it/filmclub/2019/09/049/locandinapg1.jpg");
-		theGreenKnight.put("The Green Knight", "https://upload.wikimedia.org/wikipedia/en/0/09/The_Green_Knight_poster.jpeg");
+		dune.put("titolo", "Dune");
+		dune.put("immagine", "https://mr.comingsoon.it/imgdb/locandine/235x336/55957.jpg");
+		theLastDuel.put("immagine", "https://pad.mymovies.it/filmclub/2019/09/049/locandinapg1.jpg");
+		theLastDuel.put("titolo", "The Last Duel");
+		theGreenKnight.put("immagine", "https://upload.wikimedia.org/wikipedia/en/0/09/The_Green_Knight_poster.jpeg");
+		theGreenKnight.put("titolo", "The Green Knight");
 		movieList.add(dune);
 		movieList.add(theLastDuel);
 		movieList.add(theGreenKnight);
-		model.addAllAttributes(movieList);
 		model.addAllAttributes(dune);
 		model.addAllAttributes(theGreenKnight);
 		model.addAllAttributes(theLastDuel);
+		model.addAllAttributes(movieList);
 		return "movies";
 	}
 	
 	@GetMapping("/songs")
 	public String songs(Model model) {
+		List<Map<String, String>> songList = new ArrayList<Map<String, String>>();
+		Map<String, String> waiting = new HashMap<>();
+		Map<String, String> tear = new HashMap<>();
+		Map<String, String> ifYou = new HashMap<>();
+		waiting.put("titolo", "Waiting on a War - Foo Fighters");
+		waiting.put("immagine", "https://i0.wp.com/endofacentury.it/wp-content/uploads/2021/01/image.png?resize=768%2C416&ssl=1");
+		tear.put("immagine", "https://www.revolvermag.com/sites/default/files/media/images/article/mastodonteardrinker.jpeg");
+		tear.put("titolo", "Teardrinker - Mastodon");
+		ifYou.put("immagine", "https://media.pitchfork.com/photos/6137afbe9e3d3f59b297db7c/1:1/w_500/kidamnesia.jpeg");
+		ifYou.put("titolo", "If you say the word - Radiohead");
+		songList.add(waiting);
+		songList.add(tear);
+		songList.add(ifYou);
+		model.addAllAttributes(waiting);
+		model.addAllAttributes(tear);
+		model.addAllAttributes(ifYou);
+		model.addAllAttributes(songList);
+		
 		model.addAttribute("songs", Arrays.asList("Waiting on a War - Foo Fighters", "Teardrinker - Mastodon", "If you say the word - Radiohead"));
 		return "songs";
 	}
